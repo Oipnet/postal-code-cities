a3a778f91b5a0e52962bb8a5d729d42c87f8d537
Arnaud POINTET <arnaud.pointet@gmail.com>
2024-08-14 11:40:28 +0200

    chore: Update useFetchCities to use ofetch for API requests and handle errors

--- lib/composables/useFetchCities.ts
+++ lib/composables/useFetchCities.ts
@@ -1,3 +1,4 @@
+import { ofetch } from "ofetch";
 import { City } from "../interfaces/City";
 import { FetchCitiesConfig } from "../interfaces/FetchCitiesConfig";
 
@@ -5,9 +6,11 @@ export const useFetchCities = async (postalCode: string, config: FetchCitiesConf
     const { apiUrl = 'https://geo.api.gouv.fr/communes', transformFunction = (data: any) => data } = config;
     
     const url = `${apiUrl}?codePostal=${postalCode}`;
-
-    const response = await fetch(url)
-    const jsonData = await response.json()
-    
-    return transformFunction(jsonData)
+    try {
+        const data = await ofetch(url);
+        
+        return transformFunction ? transformFunction(data) : data;
+    } catch (error) {
+        throw new Error('Erreur lors de la récupération des villes');
+    }
 }
